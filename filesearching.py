70aecc414b9a96b7cfbea9fd1e69f14b38df1ece
Himanshu Bhatnagar <33115688+Himan10@users.noreply.github.com>
2019-09-02 13:01:04 +0530

    Update filesearching.py
    -> Modules : 
            os -> os.walk(),os.join(),os.path.abspath,os.path.exists()
            re -> re.search(pattern,item),r'<pattern>'
            shutil -> shutil.send()
            sys -> sys.stdout.write(),sys.stdout.flush()
            sleep -> sleep(n)
    
    -> functions :
            waiting()
            shutil_send()
            searching_files()
            sending_files()
            show_terminal()
    
    -> Additional :
            loops
            if/else
            try/except
            continue/break/pass

--- filesearching.py
+++ filesearching.py
@@ -1,31 +1,34 @@
-# Regex  -> Searching , Sending
+""" Regex  -> Searching , Sending """
 
 import os
 import re
 import shutil
 import sys
 from time import sleep
-from pprint import pprint
 
-found_files = []
-found_paths = []
+FOUND_FILES = []
+FOUND_PATHS = []
+
 # == Searching files (extension based) with os.walk()
 
 
 def waiting():
+    """ waiting message """
     sys.stdout.write("\r")
     sys.stdout.write(" \t\t --> Processing is on the way <-- ")
     sys.stdout.flush()
 
 
 def shutil_send(source):
+    """ shutil copy function """
     try:
-        shutil.copy(source, os.path.join(os.getcwd(), "found_files"))
+        shutil.copy(source, os.path.join(os.getcwd(), "FOUND_FILES"))
     except PermissionError:
         pass
 
 
 def searching_files():
+    """ Search files using os.walk() """
     ret = 0
     ignore_list = [
         "/home/hi-man/python/pyproject",
@@ -46,45 +49,46 @@ def searching_files():
             check = re.search(regex_pattern, every_k)
             waiting()
             if check is not None:
-                found_files.append(check.group())
-                found_paths.append(os.path.join(i, check.group()))
+                FOUND_FILES.append(check.group())
+                FOUND_PATHS.append(os.path.join(i, check.group()))
             continue
     sys.stdout.write("\r \t\t --> Hurray! Searching Completed  <--\n")
     sleep(1)
-    if found_files:
+    if FOUND_FILES:
         ret = 1
     return ret
 
 
 def sending_files(send_choice):
-    if not (os.path.exists(os.path.abspath("./found_files"))):
-        print("\n Create a Directory -> found_files")
-        os.mkdir("found_files")
-    pass
+    """ Directory creation and send files """
+    if not os.path.exists(os.path.abspath("./FOUND_FILES")):
+        print("\n Creating Directory -> FOUND_FILES")
+        os.mkdir("FOUND_FILES")
+    #pass
 
     if send_choice == "yes":
-        result_file = open(os.path.abspath("./found_files/CHECK_PATHS.txt"), "w")
-        for every_path in found_paths:
+        result_file = open(os.path.abspath("./FOUND_FILES/CHECK_PATHS.txt"), "w")
+        for every_path in FOUND_PATHS:
             try:
                 result_file.write(every_path + "\n")
                 shutil_send(every_path)
                 waiting()
             except shutil.SameFileError:
-                print(" Actually there was same files ")
+                print("\n\n \t\t --> Same File Occur <--\n")
                 break
         sys.stdout.write("\r \t\t--> Hurray! Sending Complete <--\n")
     else:
         print(" --- Sending Cancelled ---")
 
-    # == Show the files at terminal
-
 
 def show_terminal(choice):
+    """ print message on the terminal """
     if choice == "names":
-        for every_file in found_files:
-            pprint(f" {found_files.index(every_file)} -> {every_file}")
+        for every_file in FOUND_FILES:
+            print(f" {FOUND_FILES.index(every_file)} -> {every_file} ")
     elif choice == "paths":
-        for every_path in found_paths:
-            pprint(f" {found_paths.index(every_path)} -> {every_path}")
+        for every_path in FOUND_PATHS:
+            print(f" {FOUND_PATHS.index(every_path)} \
+    -> {every_path} -> {os.path.getsize(every_path)}")
     else:
         print(" I'm not a stupid like you ")
